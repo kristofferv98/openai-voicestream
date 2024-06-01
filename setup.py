de98cbf11303f935059b4e8195f09ddc7e108608
kristoffervatnehol <kristoffer.vatnehol@gmail.com>
2024-06-01 19:55:00 +0200

    change the setup.py

--- setup.py
+++ setup.py
@@ -13,19 +13,19 @@ setup(
         'pyaudio',
         'httpx',
     ],
-    url='https://github.com/kristofferv98/openai-voicestream.git',
+    extras_require={
+        'dev': [
+            'pytest',
+            'flake8'
+        ]
+    },
     author='Kristoffer Vatnehol',
     author_email='kristoffer.vatnehol@gmail.com',
+    python_requires='>=3.6',
+    url='https://github.com/kristofferv98/openai-voicestream.git',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
